7c62d3a89635660eb12a13b83927226bcd936497
Ritik Kumar <ritikkumar.hesed@gmail.com>
2026-09-25 13:57:04 +0530

    add movie-details.html and  movie-details.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
-const searchForm = document.querySelector("#searchForm");
+const movieForm = document.querySelector("#movieForm");
 
 const movieInput = document.querySelector("#movieInput");
 
 const movieHub = document.querySelector("#movieHub");
-searchForm.addEventListener("submit", (e) =>{
+
+
+movieForm.addEventListener("submit", (e) =>{
+    
     e.preventDefault();
+    
     let query = movieInput.value.trim();
 
     if(!query)
@@ -18,9 +22,13 @@ searchForm.addEventListener("submit", (e) =>{
 })
 
 async function searchMovies(movieName){
+
     movieHub.innerHTML = `<div class="loader"></div>`
+    
     let response = await fetch(`http://www.omdbapi.com/?apikey=dadf8b8&s=${movieName}`);
+    
     let data = await response.json();
+    
     console.log(data);
     
     if(data.Response === "True")
@@ -28,26 +36,32 @@ async function searchMovies(movieName){
         displayMovies(data.Search);  
     }
     else{
+        console.log(data.Error);
         movieHub.innerHTML = `<p>${data.Error}</p>`
     } 
 }
 
 function displayMovies(movies){
+
     movieHub.innerHTML = "";
     
     movies.forEach((movie) =>{
-        let div = document.createElement("div");
+        const div = document.createElement("div");
 
         div.dataset.imdbID = movie.imdbID;
+        
         div.setAttribute("class", "movie-card");
-        div.innerHTML = ` <div>
+        
+        div.innerHTML = ` 
+                          <div>
                             <img src=${movie.Poster} alt="">
                           </div>
 
                           <div>
                             <p>${movie.title}</p>
                             <p>${movie.year}</p>
-                         </div> `
+                         </div> 
+                               `
 
             movieHub.append(div);
     })
@@ -59,5 +73,5 @@ movieHub.addEventListener("click", (e) =>{
     const movieCard = e.target.closest(".movie-card");
     const imdbID = movieCard.dataset.imdbID;
     // console.log(imdbID);
-    location.href = "movie-details.html";
+    location.href = `movie-details.html?id=${imdbID}`;
 })
--- movie-details.js
+++ movie-details.js
@@ -1,5 +1,7 @@
+const movieDetail = document.querySelector("#movie-detail");
 
 const params = new URLSearchParams(location.search);
+
 const imdbID = params.get("id");
 
 if(imdbID)
@@ -11,12 +13,14 @@ console.log(imdbID);
 
 async function searchMovie(){
     let response = await fetch(`http://www.omdbapi.com/?apikey=dadf8b8&i=${imdbID}&plot=full`);
+    
     let data = await response.json();
+    
     console.log(data);
     
     if(data.Response === "True")
     {
-        // displayMovies(data.Search);  
+        displayMovie(data);  
     }
     else{
         // movieHub.innerHTML = `<p>${data.Error}</p>`
@@ -24,4 +28,63 @@ async function searchMovie(){
     } 
 }
 
-// searchMovie();
+
+function displayMovie(data){
+    movieDetail.innerHTML = `
+                            <div>
+                             
+                                <img src=${data.Poster} alt="">
+                            
+                            </div>
+
+                            <div>
+                                <h2>${data.Title}</h2>
+                                <section>
+                                    <p>${data.Released}</p>
+                                    <p>${data.Rated}</p>
+                                    <p>${data.Runtime}</p>
+                                    <p>${data.Genre}</p>
+                                    <p>IMDb: ${data.imdbRating} / 10</p>
+                                </section>
+
+                                <div>
+                                    <p>Plot Overview</p>
+                                    <p>${data.Plot}</p>
+                                </div>
+
+                                <div>
+                                    <section>
+                                        <p>Director</p>
+                                        <p>${data.Director}</p>
+                                    </section>
+
+                                    <section>
+                                        <p>Writer</p>
+                                        <p>${data.Writer}</p>
+                                    </section>
+                                </div>
+
+                                <div>
+                                    <p>Actors</p>
+                                    <p>${data.Actors}</p>
+                                </div>
+
+                                 <div>
+                                    
+                                    <section>
+                                        <p>Language</p>
+                                        <p>${data.Language}</p>
+                                    </section>
+
+                                    <section>
+                                        <p>Country</p>
+                                        <p>${data.Country}</p>
+                                    </section>
+                                </div>
+
+                                <button>
+                                <a href=https://www.imdb.com/title/${data.imdbID} target="_blank" >View on IMDb</a>
+                                </button>
+
+                            </div>`
+}
